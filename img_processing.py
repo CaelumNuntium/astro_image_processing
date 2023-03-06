87af14fde628547581ea69654706ed16a36ee5d8
CaelumNuntium <st087042@student.spbu.ru>
2023-03-06 15:51:39 +0300

    First release

--- img_processing.py
+++ img_processing.py
@@ -1,13 +1,18 @@
 import pathlib
+import argparse
 import numpy
 import warnings
 from astropy.io import fits
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dir", action="store", default=".", help="Input directory")
+args = parser.parse_args()
+
 attributes = ["DATE-OBS", "TELESCOP", "INSTRUME", "OBJECT", "IMAGETYP", "START", "EXPTIME", "RA", "DEC"]
 
-fits_names = [str(_) for _ in pathlib.Path(".").glob("**/*.fits")] + [str(_) for _ in pathlib.Path(".").glob("**/*.fts")]
-fits_names = [_ for _ in fits_names if not (_.startswith("mean_") or _ == "result.fits")]
-print(f"{len(fits_names)} FITS files found")
+fits_names = [str(_) for _ in pathlib.Path(args.dir).glob("**/*.fits")] + [str(_) for _ in pathlib.Path(args.dir).glob("**/*.fts")]
+fits_names = [_ for _ in fits_names]
+print(f"{len(fits_names)} FITS files found in {args.dir}")
 fits_files = []
 i = 0
 bias_nums = []
@@ -27,24 +32,23 @@ for f in fits_names:
         for attr in attributes:
             if attr in hdu.header:
                 print(f"\t    {attr} = {hdu.header[attr]}")
-        if "IMAGETYP" in hdu.header and hdu.header["IMAGETYP"] == "bias":
-            bias_nums.append(i)
-        if "OBJECT" in hdu.header and hdu.header["OBJECT"] == "sunsky":
-            flat_nums.append(i)
+    if "IMAGETYP" in img[0].header and img[0].header["IMAGETYP"] == "bias":
+        bias_nums.append(i)
+    if "OBJECT" in img[0].header and img[0].header["OBJECT"] == "sunsky":
+        flat_nums.append(i)
+print("\n\nWarning: ONLY Primary HDUs will be processed in the next steps!")
 
-print(f"\n\nFound {len(bias_nums)} BIAS frames: ", end="")
+print(f"\nFound {len(bias_nums)} BIAS frames: ", end="")
 for _ in bias_nums:
     print(f"{_}", end=" ")
 print("\nDo you want to select other frames? (n)")
 if input() in ["y", "yes"]:
     print("\n\nSelect BIAS frames:")
     bias_nums = [int(_) for _ in input().split()]
-bias_files = [fits_files[i - 1] for i in bias_nums]
 print("\nDo you want to select DARK frames? (n)")
 if input() in ["y", "yes"]:
     print("\nSelect DARK frames:")
     dark_nums = [int(_) for _ in input().split()]
-dark_files = [fits_files[i - 1] for i in dark_nums]
 print(f"\n\nFound {len(flat_nums)} FLAT frames: ", end="")
 for _ in flat_nums:
     print(f"{_}", end=" ")
@@ -52,26 +56,43 @@ print("\nDo you want to select other frames? (n)")
 if input() in ["y", "yes"]:
     print("\nSelect FLAT frames:")
     flat_nums = [int(_) for _ in input().split()]
-flat_files = [fits_files[i - 1] for i in flat_nums]
 print("\nSelect frames with object:")
 obj_nums = [int(_) for _ in input().split()]
 obj_files = [fits_files[i - 1] for i in obj_nums]
 if len(obj_nums) == 0:
     print("\nNo frames selected!")
-    exit()
+    exit(1)
 else:
     print("\nSelected files:")
     for _ in obj_nums:
         print(fits_names[_ - 1])
 
+shape = obj_files[0][0].data.shape
+for i in range(len(obj_files)):
+    if obj_files[i][0].data.shape != shape:
+        print(f"Error: Data shape conflict in {fits_names[obj_nums[0]]} and {fits_names[obj_nums[i]]}")
+        exit(2)
+for i in bias_nums:
+    if fits_files[i][0].data.shape != shape:
+        print(f"Warning: Shape of BIAS frame in {fits_names[i]} is not equal to the object frame shape! This frame will be ignored.")
+bias_files = [fits_files[i - 1] for i in bias_nums]
+for i in dark_nums:
+    if fits_files[i][0].data.shape != shape:
+        print(f"Warning: Shape of DARK frame in {fits_names[i]} is not equal to the object frame shape! This frame will be ignored.")
+dark_files = [fits_files[i - 1] for i in dark_nums]
+for i in flat_nums:
+    if fits_files[i][0].data.shape != shape:
+        print(f"Warning: Shape of FLAT frame in {fits_names[i]} is not equal to the object frame shape! This frame will be ignored.")
+flat_files = [fits_files[i - 1] for i in flat_nums]
+
 exptime = max([img[0].header["EXPTIME"] for img in obj_files])
 datatype = numpy.dtype('f4')
 
 if len(bias_nums) > 0:
-    bias_data = numpy.ndarray(shape=bias_files[0][0].data.shape, dtype=datatype)
+    bias_data = numpy.ndarray(shape=shape, dtype=datatype)
     numpy.median([img[0].data for img in bias_files], axis=0, out=bias_data)
 else:
-    bias_data = numpy.zeros(shape=obj_files[0][0].data.shape, dtype=datatype)
+    bias_data = numpy.zeros(shape=shape, dtype=datatype)
 bias_header = fits.Header()
 bias_header["IMAGETYP"] = "bias"
 bias_header["EXPTIME"] = 0.0
@@ -79,22 +100,22 @@ bias_hdu = fits.PrimaryHDU(data=bias_data, header=bias_header)
 fits.HDUList([bias_hdu]).writeto("mean_bias.fits", overwrite=True)
 
 if len(dark_nums) > 0:
-    dark_data = numpy.ndarray(shape=dark_files[0][0].data.shape, dtype=datatype)
+    dark_data = numpy.ndarray(shape=shape, dtype=datatype)
     numpy.median([(img[0].data - bias_data) * (exptime / img[0].header["EXPTIME"]) for img in dark_files], axis=0, out=dark_data)
 else:
-    dark_data = numpy.zeros(shape=obj_files[0][0].data.shape, dtype=datatype)
+    dark_data = numpy.zeros(shape=shape, dtype=datatype)
 dark_header = fits.Header()
 dark_header["EXPTIME"] = exptime
 dark_hdu = fits.PrimaryHDU(data=dark_data, header=dark_header)
 fits.HDUList([dark_hdu]).writeto("mean_dark.fits", overwrite=True)
 
 if len(flat_nums) > 0:
-    flat_data = numpy.ndarray(shape=flat_files[0][0].data.shape, dtype=datatype)
+    flat_data = numpy.ndarray(shape=shape, dtype=datatype)
     numpy.median([(img[0].data - bias_data) * (exptime / img[0].header["EXPTIME"]) - dark_data for img in flat_files], axis=0, out=flat_data)
     valmax = numpy.max(flat_data)
     flat_data = flat_data / valmax
 else:
-    flat_data = numpy.ones(shape=obj_files[0][0].data.shape, dtype=datatype)
+    flat_data = numpy.ones(shape=shape, dtype=datatype)
 flat_header = fits.Header()
 flat_header["IMAGETYP"] = "obj"
 flat_header["OBJECT"] = "sunsky"
@@ -103,7 +124,7 @@ flat_hdu = fits.PrimaryHDU(data=flat_data, header=flat_header)
 fits.HDUList([flat_hdu]).writeto("mean_flat.fits", overwrite=True)
 
 
-obj_data = numpy.ndarray(shape=obj_files[0][0].data.shape, dtype=datatype)
+obj_data = numpy.ndarray(shape=shape, dtype=datatype)
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     if numpy.sum(numpy.where(flat_data < 0.05, 1, 0)) > 0:
@@ -114,4 +135,9 @@ obj_header["IMAGETYP"] = "obj"
 obj_header["OBJECT"] = obj_files[0][0].header["OBJECT"]
 obj_header["EXPTIME"] = exptime
 obj_hdu = fits.PrimaryHDU(data=obj_data, header=obj_header)
-fits.HDUList([obj_hdu]).writeto("result.fits", overwrite=True)
+
+print("Result file:")
+filename = input()
+if not (filename.endswith(".fts") or filename.endswith(".fits")):
+    filename = filename + ".fits"
+fits.HDUList([obj_hdu]).writeto(filename, overwrite=True)
